179ceb3a0302ccb69cb724fbfe54fb8eb718c4bd
Vitor Dourado <131503024+vitordourad0@users.noreply.github.com>
2026-05-21 20:50:56 -0300

    Enhance usuarios.sh with help and version flags
    Added help and version options to usuarios.sh

--- usuarios.sh
+++ usuarios.sh
@@ -4,5 +4,28 @@
 #obs.: lê dados do arquivo /etc/passwd
 #Vitor Dourado, Maio de 2026
 
-cut -d : -f 1,5 /etc/passwd | tr : \\t
+#Versão 1: Mostra usuários e nomes separados por TAB
+#Versão 2: Adicionado suporte a ação -h
 
+MENSAGEM_USO="
+Uso: $0 [-h]
+
+        -h      Mostra esta tela de ajuda e sai
+"
+VERSA0="
+Uso: $0 [-v]
+        -v      versao 1.0
+"
+#Tratamento das opções de linha de comando
+if test "$1" = "-h"
+then
+   echo "$MENSAGEM_USO"
+elif
+   test "$1" = "-v"
+then
+   echo "$VERSAO"
+   exit 0
+fi
+
+#Processamento
+cut -d : -f 1,5 /etc/passwd | tr : \\t
